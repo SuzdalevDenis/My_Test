c371a424bebf71f60728d3787cc191c597f0bec2
drezden <denissuzdalev15@gmail.com>
2022-08-29 23:21:29 +0300

    YES!

--- Test_1.py
+++ Test_1.py
@@ -3,12 +3,10 @@ import random
 a = '1234567890123456789012345678901234567890'
 
 Num_key = a
-length_for_key =8
-
+length_for_key =5
 
 num = ''.join(random.sample(Num_key, length_for_key))
 print("Your numbers:",num)
-
 
 key = str(input("Key:"))
 
