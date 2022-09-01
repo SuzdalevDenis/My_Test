8924d1dba5fe93ed6f266a89928fb207f5a9fc29
drezden <denissuzdalev15@gmail.com>
2022-09-01 15:08:51 +0300

    YES!

--- Test_1.py
+++ Test_1.py
@@ -1,12 +1,11 @@
 import random
 
-a = '1234567890123456789012345678901234567890'
+numbers = '1234567890123456789012345678901234567890'
 
-Num_key = a
-length_for_key =5
+length_for_key = 5
 
-num = ''.join(random.sample(Num_key, length_for_key))
-print("Your numbers:",num)
+num = ''.join(random.sample(numbers, length_for_key))
+print("Your numbers:", num)
 
 key = str(input("Key:"))
 
@@ -17,5 +16,3 @@ if key == num:
     print(Yes)
 else:
     print(No)
-
-
