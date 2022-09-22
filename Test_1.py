0061dcd4d3a55ce398fede957a554b502acb34aa
drezden <denissuzdalev15@gmail.com>
2022-09-22 11:24:27 +0300

    YES!

--- Test_1.py
+++ Test_1.py
@@ -1,32 +1,44 @@
 import random
+import time
 
-#personal = str(input('First name Last name:'))
 id_personal = str(input('Your ID:'))
-personal_list_and_id = {'22003': 'Alex Wolfski', '34556': 'Maya Doorwelle', '22002': 'Rei Ayanami', 'u': '33445'}
-
-if id_personal in personal_list_and_id:
-    print("Hello", personal_list_and_id[id_personal], "!")
-else:
-    print('Who are you? Get out!')
-
-
+id_and_personal = {
+    '22003': 'Alex Wolfski',
+    '34556': 'Maya Doorwelle',
+    '22002': 'Rei Ayanami',
+    '33445': 'Drez_Den_'
+}
 numbers = '1234567890123456789012345678901234567890'
-
 length_for_key = 6
-
 num_key = ''.join(random.sample(numbers, length_for_key))
-print("Your numbers:", num_key)
-
-key = str(input("Key:"))
 
 yes = "Welcome!"
 no = "SORRY..."
 
-if key == num_key:
-    print(yes)
-    if yes:
-        print('Yes')
+seconds = time.time()
+local = time.ctime(seconds)
+
+
+def correct_key():
+    if key == num_key:
+        print(yes)
+        if yes:
+            return'Yes'
+    else:
+        print(no)
+        if no:
+            return'No'
+
+
+if id_personal in id_and_personal:
+    print(f"Hello {id_and_personal[id_personal]}!")
+    print("Your numbers:", num_key)
+    key = str(input("Key:"))
+    print(correct_key())
+    print(local)
 else:
-    print(no)
-    if no:
-        print('No')
+    print('Who are you? Get out!')
+    print(local)
+
+
+time_list = list(local)
